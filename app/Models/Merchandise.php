9a6f15d0a5de10346b99a3ed0750811ff92de7c7
zhangrj <zhang_career@foxmail.com>
2023-07-23 19:11:11 +0800

    model of Merchandise debug

--- app/Models/Merchandise.php
+++ app/Models/Merchandise.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Merchandise extends Model
 {
+    public $timestamps = false;
+
     protected array $fillable = [
         'name',
         'description',
